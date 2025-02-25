9db1055b1421ac893d14a4cd5e50d074c1ca0980
Trithereon <100091007+Trithereon@users.noreply.github.com>
2025-02-25 16:05:46 -0500

    Add basic math functions that work in console for now

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
+// Gets the value data-action, so it can be used as input for a function. e.g. clicking "one" should send 1 to the display, etc.
+// const action = button.getAttribute('data-action'); 
+
+let firstOperand = 0;
+let operator = '';
+let secondOperand = 0;
 
 
 
-const action = button.getAttribute('data-action'); // Gets the value data-action, so that I can use it as input for a function. e.g. clicking "one" should send 1 to the display, etc.
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operate(first, op, second){
+    switch(op){
+        case '+':
+            return add(first, second);
+        case '-':
+            return subtract(first, second);
+        case '*':
+            return multiply(first, second);
+        case '/':
+            return divide(first, second);
+    };
+}
