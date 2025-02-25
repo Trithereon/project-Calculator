b1302d155927bb8e253568c061a084d7774bed77
Trithereon <100091007+Trithereon@users.noreply.github.com>
2025-02-25 15:41:47 -0500

    Make each button easily accessible by unique attributes

---
+++ script.js
@@ -0,0 +1,4 @@
+
+
+
+const action = button.getAttribute('data-action'); // Gets the value data-action, so that I can use it as input for a function. e.g. clicking "one" should send 1 to the display, etc.
